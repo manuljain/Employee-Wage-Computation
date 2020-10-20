26d1f93620735271c2622c60dee1148a9d7308e8
Manul <manuljain16@gmail.com>
2020-10-20 18:27:22 +0530

    Added Employee attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,3 +2,13 @@
 
 # Welome message
 echo "Welcome to Employee Wage Computation Program."
+
+# UC-1 Checking for employees attendance
+isPresent=1;
+randomCheck=$((RANDOM%2))
+if [ $isPresent -eq $randomCheck ]
+then
+	echo "employee is present"
+else
+	echo "employee is Absent"
+fi
