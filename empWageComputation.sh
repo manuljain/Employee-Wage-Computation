3a4af87efc7d54b12f314346831a0ebc59f49eff
Manul <manuljain16@gmail.com>
2020-10-20 18:33:26 +0530

    Added Employee daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,11 +4,20 @@
 echo "Welcome to Employee Wage Computation Program."
 
 # UC-1 Checking for employees attendance
-isPresent=1;
+# UC-2 Employee Daily wage
+isPresent=1
+wagePerhour=20
+dayhrs=8
+
 randomCheck=$((RANDOM%2))
 if [ $isPresent -eq $randomCheck ]
 then
 	echo "employee is present"
+
 else
 	echo "employee is Absent"
 fi
+
+dailywage=$(( $wagePerhour * $dayhrs ))
+echo "daily wage of employee is : $dailywage "
+
