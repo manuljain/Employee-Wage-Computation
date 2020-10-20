c888a1554f1c59dd7b19e5e33de4637d366e0bcd
Manul <manuljain16@gmail.com>
2020-10-20 18:49:01 +0530

    Added switch case statement instead of if else statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,6 +6,7 @@ echo "Welcome to Employee Wage Computation Program."
 # UC-1 Checking for employees attendance
 # UC-2 Employee Daily wage
 # UC-3 Part Time Employee wage 
+# UC-4 Using switch case statement 
 
 isPresent=1;
 isPartTime=1
@@ -15,19 +16,21 @@ wagePerhour=20
 randomCheck=$((RANDOM%2))
 if [ $isPresent -eq $randomCheck ]
 then
-	random=$((RANDOM%3))
-	if [ $isPartTime -eq $random ]
-	then
-		empHrs=4
-	echo "employee is present and working part time"
-	elif [ $isFullTime -eq $random ]
-	then
+	empCheck=$((RANDOM%3))
+	case $empCheck in
+		$isFullTime)
 		empHrs=8
 		echo "employee is present and working full time"
-	fi
+		;;
+		$isPartTime)
+		empHrs=4
+		echo "employee is present and working part time"
+		;;
+	esac
 else
 	empHrs=0
 	echo "employee is Absent"
+
 fi
 wage=$((empHrs*wagePerhour))
 echo $wage
