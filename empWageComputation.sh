af8d544abc235dad6491f36415edddc2383223e8
Manul <manuljain16@gmail.com>
2020-10-20 18:42:08 +0530

    Added part time employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,19 +5,29 @@ echo "Welcome to Employee Wage Computation Program."
 
 # UC-1 Checking for employees attendance
 # UC-2 Employee Daily wage
-isPresent=1
+# UC-3 Part Time Employee wage 
+
+isPresent=1;
+isPartTime=1
+isFullTime=2
 wagePerhour=20
-dayhrs=8
 
 randomCheck=$((RANDOM%2))
 if [ $isPresent -eq $randomCheck ]
 then
-	echo "employee is present"
-
+	random=$((RANDOM%3))
+	if [ $isPartTime -eq $random ]
+	then
+		empHrs=4
+	echo "employee is present and working part time"
+	elif [ $isFullTime -eq $random ]
+	then
+		empHrs=8
+		echo "employee is present and working full time"
+	fi
 else
+	empHrs=0
 	echo "employee is Absent"
 fi
-
-dailywage=$(( $wagePerhour * $dayhrs ))
-echo "daily wage of employee is : $dailywage "
-
+wage=$((empHrs*wagePerhour))
+echo $wage
